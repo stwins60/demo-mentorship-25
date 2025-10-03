5cc7e1d540d0c398828c17b90d4cd08b9cad5207
Idris Fagbemi <idrisniyi94@gmail.com>
2025-10-02 20:48:22 -0500

    new code

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "What is your name"
+read name
+
+echo "My name is $name"
+
+echo "Do you want to create a directory ? (true/false)"
+read option
+
+if [[ $option == "true" ]]; then
+   echo "Enter the name of the directory to create: "
+   read dir_name
+   mkdir "$dir_name"
+   echo "Directory '$dir_name' created."
+else
+   echo "Have a nice day"
+   exit 1
+fi
